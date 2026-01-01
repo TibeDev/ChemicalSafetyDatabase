a86648b77e8dc11f11262d19f7e9c7fdf75837d8
TibeDev <tibevdbrande@gmail.com>
2026-01-01 17:52:00 +0100

    UI change and Data file

--- src/app.js
+++ src/app.js
@@ -12,3 +12,56 @@ function CancelChemical() {
   emptyState.style.display = "block";
   chemicalForm.style.display = "none";
 }
+
+fetch("../data/sdsData.json")
+  .then((res) => res.json())
+  .then((data) => {
+    LoadHazards(data.hazards);
+    LoadPhrases("h-phrases", data.hPhrases);
+    LoadPhrases("p-phrases", data.pPhrases);
+    LoadHazardClasses("select-hazard-classes", data.hazardClasses);
+    LoadHazardClasses("select-phyiscal-states", data.physicalStates);
+  });
+
+function LoadHazards(hazards) {
+  const path = "../images/GHS_ICONS/";
+  const container = document.getElementsByClassName("hazard-grid")[0];
+  hazards.forEach((hazard) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+    <label id="hazard">
+      <input type="checkbox" />
+      <img src="${path}${hazard}" />
+    </label>
+    `
+    );
+  });
+}
+
+function LoadHazardClasses(containerId, classes) {
+  const container = document.getElementById(containerId);
+  classes.forEach((c) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <option value="${c}">${c}</option>
+    `
+    );
+  });
+}
+
+function LoadPhrases(containerId, phrases) {
+  const container = document.getElementById(containerId);
+  phrases.forEach((phrase) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <label id="phrase">
+        <input type="checkbox" />
+        <p>${phrase.code} - ${phrase.text}</p>
+      </label>
+    `
+    );
+  });
+}
